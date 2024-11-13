6d9667fffc105be3b7544943a529265d29da0e01
Marco Alonso <marcoalonsorota@hotmail.com>
2024-11-13 10:17:50 -0600

    Initial Commit

---
+++ FootbalSports/FootbalSportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootbalSportsApp.swift
+//  FootbalSports
+//
+//  Created by Marco Alonso on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FootbalSportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
